4c8e0648110fc78fa53947ef327ef9219d6625d5
xxljunjun <liaojunjun@autel.com>
2021-07-27 18:08:17 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+import store from './store'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App,
+  store
+})
+app.$mount()
---
+++ src/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="home">
+    <TabBar />
+  </view>
+</template>
+
+<script>
+import TabBar from '@/components/tabBar.vue'
+export default {
+  data() {
+    return {
+      token: '',
+    }
+  },
+  mounted() {},
+  onLoad() {},
+  methods: {},
+  components: {
+    TabBar,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  color: #000;
+}
+</style>
